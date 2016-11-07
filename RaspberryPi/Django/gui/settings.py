3846f801268cf1d96fa1fd338c0145306820bf2e
Lee SeungRyeol <toy0605@gmail.com>
2016-11-07 23:03:08 +0900

    장고 프레임워크 디버그모드로 변경

--- RaspberryPi/Django/gui/settings.py
+++ RaspberryPi/Django/gui/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&jrf89r)rv%&ua5$!9hni94h0cm@qefol^!$6gjtf_o+y44mi1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = '*'
 
