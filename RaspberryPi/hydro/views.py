b6bf20b27a83efd2891e3a0decc23be14fa5ab8b
toy0605 <toy0605@gmail.com>
2016-11-09 22:28:44 +0900

    장고 프레임워크 수정

--- RaspberryPi/hydro/views.py
+++ RaspberryPi/hydro/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+class status(request):
+    return render(request, 'hydro/status.html', {})
+
+class ajax_status(request):
+    return HttpResponse()
