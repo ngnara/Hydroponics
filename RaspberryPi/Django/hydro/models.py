fbe142a87f282304eda4089212b06a12f8576e9c
toy0605 <toy0605@gmail.com>
2016-11-09 14:51:47 +0900

    모델 추가

--- RaspberryPi/Django/hydro/models.py
+++ RaspberryPi/Django/hydro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class SensorLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField(default=timezone.now)
+    temp = models.FloatField()
+    humid = models.FloatField()
+    light = models.IntegerField()
+    ph = models.FloatField()
+    
+    def __str__(self):
+        return self.id
+   
