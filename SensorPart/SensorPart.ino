1cf53577feabeacabfa9b0bd25dc91b4bc906080
toy0605 <toy0605@gmail.com>
2016-11-10 11:54:20 +0900

    아두이노 합본

--- SensorPart/SensorPart.ino
+++ SensorPart/SensorPart.ino
@@ -3,8 +3,12 @@
 
 #define DHT_PIN 2
 #define DHT_TYPE DHT22
-#define LIGHT_PIN A0;
+#define LIGHT_PIN A0
+#define PH_METER_PIN A1
+#define PH_ARRAY_LENGTH 40
 
+int pHArray[PH_ARRAY_LENGTH];
+int pHArrayIndex = 0;
 int DelayInterval = 10000;
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -37,13 +41,14 @@ void initSerial()
 }
 
 // 센서 값을 Json으로 변경
-JsonObject &toJson(float temp, float humid, int light) {
+JsonObject &toJson(float temp, float humid, int light, float ph) {
   DynamicJsonBuffer jsonBuffer;
   JsonObject &jsonObj = jsonBuffer.createObject();
  
   jsonObj["Temp"] = temp;
   jsonObj["Humid"] = humid;
   jsonObj["Light"] = light;
+  jsonObj["pH"] = ph;
 
   return jsonObj;
 }
@@ -64,6 +69,57 @@ void serialEvent() {
   }
 }
 
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+
+float readpH () { 
+      float pHValue, voltage;
+      pHArray[pHArrayIndex++] = analogRead(PH_METER_PIN);
+      if(pHArrayIndex == PH_ARRAY_LENGTH )
+        pHArrayIndex=0;
+      voltage = avergearray(pHArray, PH_ARRAY_LENGTH) * 5.0 / 1024;
+      pHValue = 3.5 * voltage + 0.0;
+
+      return pHValue;
+}
+
 void setup() {
   initSerial();
   dht.begin();
@@ -73,8 +129,9 @@ void loop() {
   float tmp = dht.readTemperature();
   float hum = dht.readHumidity();
   int light = analogRead(LIGHT_PIN);
+  float ph = readpH();
   
-  toJson(tmp, hum, light).printTo(Serial);
+  toJson(tmp, hum, light, ph).printTo(Serial);
   Serial.println("");
   delay(DelayInterval);
 }
