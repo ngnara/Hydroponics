09ad1299ac11b30d43380ec36f32dcf2406555a4
toy0605 <toy0605@gmail.com>
2016-11-11 11:56:53 +0900

    수정

--- Server/hydro/views.py
+++ Server/hydro/views.py
@@ -6,7 +6,7 @@ import json;
 # Create your views here.
 
 def ajaxStatus(request):
-    log = SensorLog.objects.all()[:1].order_by('date')
+    log = SensorLog.objects.all().order_by('date')[:1]
     jsonData = { "Temp" : log.temp, "Humid" : log.humid, "Light" : log.light, "pH" : log.ph }
     jsonString = json.dumps(jsonData)
     return HttpResponse(jsonString, content_type="application/json")
